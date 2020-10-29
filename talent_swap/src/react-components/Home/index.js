3837b903a7f88d5ca6373f971e63cebe265edbab
gabejona <jonathan.gabe@mail.utoronto.ca>
2020-10-29 13:14:22 -0400

    Small css fix

--- talent_swap/src/react-components/Home/index.js
+++ talent_swap/src/react-components/Home/index.js
@@ -11,20 +11,20 @@ class Home extends React.Component {
         {/* Header component(navbar). need login state check */}
         <Header/>
         <div className="mission" id="top">
-          <h1>Welcome to Talent Swap</h1>
+          <h1 className="ltitle">Welcome to Talent Swap</h1>
           <p className="mstatement"> Our mission at Talent Swap is to engage a community of teachers and learners to exchange knowledge. Teachers seeking to educate or instruct can offer courses to users on our platform.
           Learners can take interesting courses available on our platform. We provide users the medium and tools to lifelong learn.
           </p>
         </div>
         <div className="mission">
           <p className="teach">
-            <h1>Teach</h1>
+            <h1 className="ltitle">Teach</h1>
             <p className="mstatement"> Share your wisdom with other users through one on one instruction or in a class setting with multiple
             users.  
             </p>
           </p>
           <p className="learn">
-            <h1>Learn</h1>
+            <h1 className="ltitle">Learn</h1>
             <p className="mstatement"> Continue lifelong learning by taking classes in fascinating courses from other users.  
             </p>
           </p>
