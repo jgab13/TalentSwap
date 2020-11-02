99e1bab0c02d8128bd679dbba2847107203e1b7f
jayruin <28711665+jayruin@users.noreply.github.com>
2020-11-02 17:35:40 -0500

    Check for banned user in UserManager.

--- talent_swap/src/users/user-manager.js
+++ talent_swap/src/users/user-manager.js
@@ -1,4 +1,5 @@
 import User from "./user";
+import AdminUser from "./admin-user";
 
 const hardCodedUsers = [
     new User({
@@ -26,12 +27,19 @@ const hardCodedUsers = [
 class UserManager {
     static login(username, password) {
         // Needs server call
+        let admin = new AdminUser();
+        let userToReturn;
         if (username === "user" && password === "user") {
-            return hardCodedUsers[0];
+            userToReturn = hardCodedUsers[0];
         } else if (username === "user1" && password === "user1") {
-            return hardCodedUsers[1];
+            userToReturn = hardCodedUsers[1];
         } else if (username === "admin" && password === "admin") {
-            return "something";
+            return admin;
+        }
+        if (admin.getBannedUsers().includes(userToReturn)) {
+            throw new Error("USER IS BANNED");
+        } else {
+            return userToReturn;
         }
     }
 
